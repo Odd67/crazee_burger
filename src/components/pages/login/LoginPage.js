c538b7e6a98529c02176fba2893f05036d6a7aaf
Odd67 <hoursk47@gmail.comm>
2022-10-15 12:01:46 +0200

    ♻️ F01: LOGIN > Create LoginForm

--- src/components/pages/login/LoginPage.js
+++ src/components/pages/login/LoginPage.js
@@ -1,29 +1,5 @@
-import { useState } from "react";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleChange = (event) => setInputValue(event.target.value);
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(`Bonjour ${inputValue}`);
-    setInputValue("");
-  };
-
-  return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <br />
-      <h2>Connectez-vous</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={inputValue}
-          onChange={handleChange}
-          required
-          placeholder="Entrez votre prénom"
-        />
-        <button>Accéder à mon espace</button>
-      </form>
-    </div>
-  );
+  return <LoginForm />;
 }
