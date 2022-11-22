c0e29ff590487c73ab442f41ddab222f5632324f
Odd67 <hoursk47@gmail.comm>
2022-11-22 12:02:54 +0100

    ♻️ F03: LoginForm > Create reusable <PrimaryButton/>

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import TextInput from "../../reusable-ui/TextInput";
 import { BsPersonCircle } from "react-icons/bs";
+import PrimaryButton from "../../reusable-ui/PrimaryButton";
 import { IoChevronForward } from "react-icons/io5";
 
 export default function LoginForm() {
@@ -24,10 +25,10 @@ export default function LoginForm() {
         required
         placeholder="Entrez votre prénom"
       />
-      <button className="button-with-icon">
-        <span>Accéder à mon espace</span>
-        <IoChevronForward className="icon" />
-      </button>
+      <PrimaryButton
+        label="Accéder à mon espace"
+        Icon={<IoChevronForward className="icon" />}
+      />
     </LoginFormStyled>
   );
 }
@@ -57,49 +58,11 @@ const LoginFormStyled = styled.form`
     margin: 20px 10px 10px;
   }
 
-  .button-with-icon {
-    width: 100%;
-    display: inline-flex;
+  .icon {
+    display: flex;
     justify-content: center;
     align-items: center;
-    position: relative;
-
-    line-height: 1;
-    padding: 18px 24px;
-    border-radius: 5px;
-    white-space: nowrap;
-    text-decoration: none;
-
     font-size: 15px;
-    font-weight: 800;
-    color: white;
-    background-color: #ff9f1b;
-    border: 1px solid #ff9f1b;
-
-    &:hover:not(:disabled) {
-      background-color: white;
-      color: #ff9f1b;
-      border: 1px solid #ff9f1b;
-      transition: all 200ms ease-out;
-    }
-
-    &:active {
-      color: white;
-      background-color: #ff9f1b;
-      border: 1px solid #ff9f1b;
-    }
-
-    &:disabled {
-      opacity: 0.6;
-      cursor: not-allowed;
-    }
-
-    .icon {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 15px;
-      margin-left: 10px;
-    }
+    margin-left: 10px;
   }
 `;
