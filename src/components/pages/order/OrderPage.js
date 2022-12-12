3ff1b761030d1245e99c9456cab7c62cafa87054
Odd67 <hoursk47@gmail.comm>
2022-12-12 15:29:56 +0100

    ♻️ F04: ORDER > create Navbar & Main

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,5 +1,7 @@
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import Main from "./Main";
+import Navbar from "./Navbar";
 
 export default function OrderPage() {
   const { username } = useParams();
@@ -7,14 +9,8 @@ export default function OrderPage() {
   return (
     <OrderPageStyled>
       <div className="container">
-        <div className="navbar">
-          Navbar
-          <h1>Bonjour {username}</h1>
-          <Link to="/">
-            <button>Déconnexion</button>
-          </Link>
-        </div>
-        <div className="main">Main</div>
+        <Navbar username={username} />
+        <Main />
       </div>
     </OrderPageStyled>
   );
@@ -33,15 +29,5 @@ const OrderPageStyled = styled.div`
     width: 1400px;
     display: flex;
     flex-direction: column;
-
-    .navbar {
-      background: blue;
-      height: 10vh;
-    }
-
-    .main {
-      background: green;
-      flex: 1;
-    }
   }
 `;
